7f254d1c9fd95198546d40a536d28a77a1dea772
Rutvik Sutaria <rutvik.sutaria1@gmail.com>
2020-02-17 11:16:48 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname,'client/build')));
+app.get('/api/passwords',(req,res,next) => {
+    const passwords = [1,2,3,4,5];
+    res.json(passwords);
+    console.log('Sent a json array.');
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+console.log(`Listening on ${port}`);
